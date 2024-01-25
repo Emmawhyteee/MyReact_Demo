3783257c739ed6add694298218e7ba99843349b3
Nwankwo Emmanuel <emmatexiii@gmail.com>
2024-01-25 19:35:05 +0100

    modifications to team section

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import '../App.css'
-import img1 from '../images/mobile.png'
+import React from "react";
+import "../App.css";
+import img1 from "../images/mobile.png";
 
 function Home() {
- 
   return (
     <>
-      <div className='section'>
-        <div className='left-section' id="content">
-          <h1>Hello World!<br />
-           React learning is lovely and I didnt know it was this easy <br />
-           I also leant some new things that I shouldn't focus on past knowledge <br />
-           that I should also learn to read always
-        </h1>
-        <div className='btnn'>Get Started</div>
+      <div className="section">
+        <div className="left-section" id="content">
+          <h1>
+            Hello World!
+            <br />
+            React learning is lovely and I didnt know it was this easy <br />
+            I also leant some new things that I shouldn't focus on past
+            knowledge <br />
+            that I should also learn to read always
+          </h1>
+          <div className="btnn">Get Started</div>
         </div>
-        <div className='right-section' id='content1'>
-       <img src={img1} alt='phone-view' height='500px' />
+        <div className="right-section" id="content1">
+          <img src={img1} alt="phone-view" height="500px" />
         </div>
       </div>
-
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
