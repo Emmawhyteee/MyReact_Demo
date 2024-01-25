f9fd5d5d29d54bef95ee499619c726da4c8aef3e
Nwankwo Emmanuel <emmatexiii@gmail.com>
2024-01-25 19:12:06 +0100

    adjustments on the Teams Section

--- src/sections/Team.jsx
+++ src/sections/Team.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../App.css'
-
+import img_T from '../images/Team.jpg'
 
 function Team() {
  
@@ -9,7 +9,26 @@ function Team() {
   return (
       <>
            <div className='section'>
-     <h1>Team page</h1></div>
+        <h1>Team page</h1></div>
+      <p className='Team_L'> Our tech team is not just a group of individuals;
+        we are a collective force driving innovation, solving complex challenges, and shaping the future of technology. Comprising highly skilled professionals with diverse expertise, our team brings a wealth of knowledge
+        and a passion for cutting-edge solutions to every project.</p>
+      <div className='Team_Mum'>
+        <div id='left_T'>
+          {/* i tried using anything here as a tag,
+          and it still worked when i called it in css */}
+          <img src={img_T} alt="Team_member" width={'250px'} /> <br />
+         <text> M.D</text>
+        </div>
+        <div id='center_T'>
+          <img src={img_T} alt="Team_member" width={'250px'} /> <br />
+          <text>Team lead</text>
+        </div>
+        <div id='Right_T'>
+          <img src={img_T} alt="Team_member" width={'250px'} /> <br />
+          <text> Developers</text>
+        </div>
+      </div>
     </>
   )
 }
